864cbe6cc335942936882bc455202e699e99d74b
hs <1519706548@qq.com>
2026-05-25 15:58:28 +0800

    init local service ai backend project

---
+++ src/test/java/com/hs/localserviceai/LocalServiceAiBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hs.localserviceai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocalServiceAiBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
